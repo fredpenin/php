58a5d0a9370837ad701a783752a876ccee43be23
fredpenin <fred.penin@gmail.com>
2018-10-24 14:24:17 +0200

    Create structure Pizzastore
    structure de base du projet de pizzeria / PDO

---
+++ 13-pizzastore/partials/footer.php
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- script jquery -->
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <!-- script popper  -->        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+        <!-- script bootstrap -->
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        <!-- my script -->
+        <script src="assets/js/script.js"></script>
+</body>
+</html>
---
+++ 13-pizzastore/partials/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- CDN Bootstrap style -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <!-- my style -->
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+</head>
+
+<body>
